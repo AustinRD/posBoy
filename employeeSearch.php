7f151dc858d458adc37dfdb54f327225243e0f01
Kevin Fields <fieldsk2@wyvern.acsl.newpaltz.edu>
2020-04-23 16:59:52 -0400

    added styles for tables

--- employeeSearch.php
+++ employeeSearch.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false){
-        header("Location: login.php");
+	header("Location: login.php");
 }
 
 ?>
@@ -11,72 +11,67 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false){
 require_once 'config.php';
 require_once 'db.php';
 
-$db = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+$db =connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
 
+$search_result = '';
 
-$output ='';
-
-if(isset($_POST['search'])) {
-   $searchq = $_POST['search'];
-   $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
-
-   $query = mysqli_query($db, "SELECT * FROM EmployeeDatabase WHERE EmployeeID LIKE '%$searchq%' OR FirstName LIKE '%
-$searchq%' OR LastName LIKE '%$searchq%' OR BankAccountNum LIKE '%$searchq%' OR RoutingNum LIKE '%$searchq%' OR Addre
-ss LIKE '%$searchq%' OR PhoneNumber LIKE '%$searchq%' OR PlaintextPass LIKE '%$searchq%'" ) or die("could not search"
-);
-   $count = mysqli_num_rows($query);
-   if($count == 0){
-        $output = 'There were no search results';
-   }else{
-        while($row = mysqli_fetch_array($query)){
-           $ID = $row['EmployeeID'];
-           $fname = $row['FirstName'];
-           $lname = $row['LastName'];
-           $BankAccountNum = $row['BankAccountNum'];
-           $RoutingNum = $row['RoutingNum'];
-           $address = $row['Address'];
-           $phone = $row['PhoneNumber'];
-           $pass = $row['PlaintextPass'];
-
-           $output .= '<div>'.$ID.' '.$fname.' '.$lname.' '.$BankAccountNum.' '.$RoutingNum.' '.$address.' '.$phone.'
- '.$pass.'</div>';
-   }
+if(isset($_POST['search'])){
+$valueToSearch=$_POST['valueToSearch'];
+$valueToSearch= preg_replace("#[^0-9a-z]#i","",$valueToSearch);
+$search_result=mysqli_query($db, "SELECT * FROM EmployeeDatabase WHERE EmployeeID LIKE '%$valueToSearch%' OR FirstName LIKE '%$valueToSearch%' OR LastName LIKE '%$valueToSearch%' OR BankAccountNum LIKE '%$valueToSearch%' OR RoutingNum LIKE '%$valueToSearch%' OR Address LIKE '%$valueToSearch%' OR PhoneNumber LIKE '%$valueToSearch%'") or die("No Search Results");
 }
+else{
+$search_result=mysqli_query($db, "SELECT * FROM EmployeeDatabase");
 }
 ?>
-
 
 <html>
 <link rel="stylesheet" type="text/css" href="mystyle.css">
 <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
 <div class="screen">
-<head>
-<h3>[Employee Search]</h3>
-<nav>
-            <a href="main.php">Home</a> |
-            <a href="inventory.php">Inventory</a> |
-            <a href="sales.php">Sales<a> |
-            <a href="customerSearch.php">Customer Search<a> |
-            <a href="EmployeeSearch.php">Employee Search<a> |
-            <a href="checkout.php">Checkout</a>
-</nav>
-</head>
+
+<?php
+echo '<head><h3>[Employee Search]</h3>'
+    . $_SESSION['navbar']
+    . '</head>';
+?>
 <body>
 <br>
 
-<form action="EmployeeSearch.php" method="post">
-        <input type="text" name="search" placeholder="search"/>
-        <input type="submit" value="search"/>
-</form>
-<?php print("$output"); ?>
+<form action="employeeSearch.php" method="post">
+<input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
+<input type="submit" name="search" value="filter"><br><br>
+<table border='1'>
+<tr>
+<th>EmployeeID</th>
+<th>FirstName</th>
+<th>LastName</th>
+<th>BankAccountNumber</th>
+<th>RoutingNumber</th>
+<th>Address</th>
+<th>PhoneNumber</th>
+</tr>
+
+<?php while($row = mysqli_fetch_array($search_result)):?>
+<tr>
+<td><?php echo $row['EmployeeID'];?></td>
+<td><?php echo $row['FirstName'];?></td>
+<td><?php echo $row['LastName'];?></td>
+<td><?php echo $row['BankAccountNum'];?></td>
+<td><?php echo $row['RoutingNum'];?></td>
+<td><?php echo $row['Address'];?></td>
+<td><?php echo $row['PhoneNumber'];?></td>
+</tr>
+<?php endwhile;?>
+</table>
 
 <div>
 <?php
-        if(@$_GET['ErrorMess']==true){
+	if(@$_GET['ErrorMess']==true){
 ?>
-        <div class="addText"><?php echo $_GET['ErrorMess'] ?></div>
+	<div class="addText"><?php echo $_GET['ErrorMess'] ?></div>
 <?php
-                }
+		}
 ?>
 </body>
 </div>
