4bd288cf219a04d3855d02465dac8d8f5d62bae2
Geoffrey Mcglinn <mcglinng1@wyvern.acsl.newpaltz.edu>
2020-05-09 12:43:59 -0400

    added store map to index

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <body>
     <p>Welcome &#60;Valued Customer&#62; to &#60;Copywritten Store Name&#62;!</p>
 
-<br/><br/><br/><br/>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/cco_DvSzrts?autoplay=1" frameborder="0" allow="ac
-celerometer; &autoplay=1; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<br/><br/><br/>
+<img src="https://i.imgur.com/InXJwHe.jpg"alt="Store Map"width=600 height=500>
+
         <p><br/><br/><a href="login.php">&#62;Employee Login</a></p>
 </body>
 
