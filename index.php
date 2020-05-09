52a79eaafdc8f5c2faf266c7abb24fae01fa88e2
Geoffrey Mcglinn <mcglinng1@wyvern.acsl.newpaltz.edu>
2020-05-09 13:00:21 -0400

    made store map into a png

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <p>Welcome &#60;Valued Customer&#62; to &#60;Copywritten Store Name&#62;!</p>
 
 <br/><br/><br/>
-<img src="https://i.imgur.com/InXJwHe.jpg"alt="Store Map"width=600 height=500>
+<img src="https://i.imgur.com/CxjAnbb.png"alt="Store Map"width=600 height=500>
 
         <p><br/><br/><a href="login.php">&#62;Employee Login</a></p>
 </body>
